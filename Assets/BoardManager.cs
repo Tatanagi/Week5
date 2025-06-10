efe9f5a4efb308d34c47feb40af14c5d13f4c6c4
Tatanagi <kayrafaelchester1925@gmail.com>
2025-06-10 17:52:28 +0800

    Updated

--- Assets/BoardManager.cs
+++ Assets/BoardManager.cs
@@ -1,11 +1,22 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoardManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject[] tilePrefabs;
+    public GameObject housePrefabs;
+    public GameObject capsulePrefab;
+    public GameObject treePrefab;
+    public Text TextScore;
+    GameObject[] tiles;
+
     public List<GameObject> spawnedTiles = new List<GameObject>();
+
+    long dirtBB = 0;
+    long treeBB = 0;
 
     void Start()
     {
@@ -18,14 +29,25 @@ public class BoardManager : MonoBehaviour
         for (int r = 0; r < 8; r++)
             for (int c = 0; c < 8; c++)
             {
-                int randomTile = Random.Range(0, tilePrefabs.Length);
+                int randomTile = UnityEngine.Random.Range(0, tilePrefabs.Length);
                 Vector3 pos = new Vector3(c, 0, r);
                 GameObject tile = Instantiate(tilePrefabs[randomTile], pos, Quaternion.identity);
                 tile.name = tile.tag + "_" + r + "_" + c;
+
+                if (tile.tag == "Dirt")
+                {
+                    dirtBB = SetCellState(dirtBB, r, c);
+                    PrintBB("Dirt", dirtBB);
+                }
                 spawnedTiles.Add(tile);
             }
+        Debug.Log("Dirt Cells = " + CellCount(dirtBB));
     }
-
+    void PrintBB(string name, long BB)
+    {
+        Debug.Log(name + ": " + Convert.ToString(BB, 2).PadLeft(64, '0'));
+        InvokeRepeating("PlantTree", 0.25f, 0.25f);
+    }
     public void DeleteBoard()
     {
         for (int i = 0; i < spawnedTiles.Count; i++)
@@ -34,5 +56,42 @@ public class BoardManager : MonoBehaviour
                 DestroyImmediate(spawnedTiles[i].gameObject);
         }
         spawnedTiles.Clear();
+    }
+
+    long SetCellState(long Bitboard, int row, int col)
+    {
+        long newBit = 1L << (row * 8 + col);
+        return (Bitboard |= newBit);
+    }
+
+    bool GetCellState(long Bitboard, int row, int col)
+    {
+        long mask = 1L << (row * 8 + col);
+        return ((Bitboard & mask) != 0);
+    }
+
+    int CellCount(long bitboard)
+    {
+        int count = 1;
+        long bb = bitboard;
+        while (bb != 0)
+        {
+            bb &= bb - 1;
+            count++;
+        }
+        return count;
+    }
+
+    void PlantTree()
+    {
+        int rr = UnityEngine.Random.Range(0, 8);
+        int rc = UnityEngine.Random.Range(0, 8);
+        if (GetCellState(dirtBB, rr, rc))
+        {
+            GameObject tree = Instantiate(treePrefab);
+            tree.transform.parent = tiles[rr * 8 + rc].transform;
+            tree.transform.localPosition = Vector3.zero;
+            treeBB = SetCellState(treeBB, rr, rc);
+        }
     }
 }
